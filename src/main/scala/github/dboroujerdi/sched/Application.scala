58526926ef4e8d1086b6527388f71bfd2c070f3a
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2016-11-12 10:08:17 +0000

    akka stream parse implementation

--- src/main/scala/github/dboroujerdi/sched/Application.scala
+++ src/main/scala/github/dboroujerdi/sched/Application.scala
@@ -2,16 +2,21 @@ package github.dboroujerdi.sched
 
 import github.dboroujerdi.sched.config.DefaultConfigComponent
 import github.dboroujerdi.sched.infrastructure.DefaultActorSystemComponent
+import github.dboroujerdi.sched.model.Types.Schedule
 import github.dboroujerdi.sched.parse.SynchronousParserComponent
+import github.dboroujerdi.sched.parse.pool.ParallelParserComponent
+import github.dboroujerdi.sched.parse.stream.StreamParseComponent
 import github.dboroujerdi.sched.poller.PollExecutorComponent
 import github.dboroujerdi.sched.scraping.{WebBrowserComponent, WebScraperComponent}
 import github.dboroujerdi.sched.sse.{StreamPublisherComponent, WebServerComponent}
 
+import scala.concurrent.Future
+
 trait Application extends PollExecutorComponent
+  with DefaultActorSystemComponent
   with WebScraperComponent
   with WebBrowserComponent
-  with SynchronousParserComponent
-  with DefaultActorSystemComponent
+  with StreamParseComponent
   with StreamPublisherComponent
   with WebServerComponent
   with DefaultConfigComponent {
@@ -19,9 +24,12 @@ trait Application extends PollExecutorComponent
   taskExecutor.start { () =>
     println("Scrape task executing..")
 
-    val events = scraper.scrape(config.getString("schedule.scrape.url"))
+    val events: Future[Schedule] = scraper.scrape(config.getString("schedule.scrape.url"))
 
-    publish(events)
+    events.onSuccess {
+      case schedule => publish(schedule)
+        // todo is this exhaustive?
+    }
   }
 
   startServer()
---
+++ src/main/scala/github/dboroujerdi/sched/parse/stream/StreamParseComponent.scala
@@ -0,0 +1,47 @@
+package github.dboroujerdi.sched.parse.stream
+
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
+import akka.util.Timeout
+import cats.data.Xor
+import github.dboroujerdi.sched.infrastructure.ActorSystemComponent
+import github.dboroujerdi.sched.model.ScheduledEvent
+import github.dboroujerdi.sched.model.Types.Schedule
+import github.dboroujerdi.sched.parse._
+import net.ruippeixotog.scalascraper.model.{Document, Element}
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
+
+trait StreamParseComponent extends ParserComponent {
+  this: ActorSystemComponent =>
+
+  class StreamParser extends Parser {
+
+    private val htmlParser = new HtmlScheduleParser(TimeParser)
+    val parser = new MatchElementParser(TimeParser)
+
+    implicit val timeout: Timeout = 5 seconds
+
+    val parserFlow =
+      Flow[Element]
+        .map(parser.parseElement)
+        .filter {
+          case Xor.Left(error@ExceptionalScrapeError(_, _)) => false
+          case _ => true
+        }
+        .collect {
+          case Xor.Right(event) => event
+        }
+
+    def parse(doc: Document): Future[Schedule] = {
+      val elements: Seq[Element] = htmlParser.parseMatchElements(doc)
+
+      Source(elements.toList)
+        .via(parserFlow)
+        .toMat(Sink.seq[ScheduledEvent])(Keep.right)
+        .run()
+    }
+  }
+
+  val parser: Parser = new StreamParser
+}
